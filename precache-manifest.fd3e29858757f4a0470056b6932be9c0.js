0925323ba8055c8f3858e1d195165fc3c0b5771e
bgu7421 <60065218+bgu7421@users.noreply.github.com>
2023-09-18 21:17:43 +0900

    Add files via upload

---
+++ precache-manifest.fd3e29858757f4a0470056b6932be9c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e3fafc63c85ff3d7256524bb5ec06b3",
+    "url": "/Modstore.ai/index.html"
+  },
+  {
+    "revision": "b6b0298fcc5c050d3c01",
+    "url": "/Modstore.ai/static/css/main.28147aff.chunk.css"
+  },
+  {
+    "revision": "7eef763ca6355cb68967",
+    "url": "/Modstore.ai/static/js/2.1c9920aa.chunk.js"
+  },
+  {
+    "revision": "4d74ce1e7fee0a16b28267ca01d74155",
+    "url": "/Modstore.ai/static/js/2.1c9920aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b0298fcc5c050d3c01",
+    "url": "/Modstore.ai/static/js/main.45d273b8.chunk.js"
+  },
+  {
+    "revision": "869b1edebc9bfbcf3d46",
+    "url": "/Modstore.ai/static/js/runtime-main.faa5e141.js"
+  },
+  {
+    "revision": "da4be93643157af365d309b81858e545",
+    "url": "/Modstore.ai/static/media/logo-vertical-main-page.da4be936.png"
+  },
+  {
+    "revision": "0217ed6efa52727a24e453c04f775a6e",
+    "url": "/Modstore.ai/static/media/product-image.0217ed6e.png"
+  }
+]);
